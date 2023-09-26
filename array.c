a736e618119284a8f3f3d63b808b68737cc4f8e5
itbindu <120378233+itbindu@users.noreply.github.com>
2023-09-26 11:51:50 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[5],i;
+printf("enter the numbers :");
+ for(i=0;i<=4;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ for(i=0;i<=4;i++)
+ {
+ printf("%d\n",a[i]);
+ }
+ return;
+ }
+ 
