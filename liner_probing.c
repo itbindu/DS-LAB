e524850931d5a16c613340cae6f840890073b608
itbindu <120378233+itbindu@users.noreply.github.com>
2023-11-21 12:13:56 +0530

    Add files via upload

---
+++ liner_probing.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int a[100],n,m,key,h,x,i;
+int k=0;
+printf("Enter the array size: ");
+scanf("%d",&n);
+for(i=0;i<=n-1;i++){
+a[i]=0;}
+for(int j=0;j<n;j++){
+printf("\n Enter the key: ");
+scanf("%d",&key);
+x=key%n;
+if(a[x]==0){
+a[x]=key;
+}
+else {
+do
+{
+k++;
+}
+while(a[(x+k)%n]!=0);
+a[((x+k)%n)]=key;
+}
+}
+for(int i=0;i<n;i++){
+printf("%d\t",a[i]);
+}
+return 0;
+}
+
